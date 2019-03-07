31f2c9c73974e85e87f57af43098f8280f81ec2e
Miltan <34897878+miltann@users.noreply.github.com>
2019-03-07 21:50:51 +0100

    minor bugfix
    updated sql query: now shows all inactive admins

--- echelon/active.php
+++ echelon/active.php
@@ -50,7 +50,7 @@ $start_row = $page_no * $limit_rows;
 $query = sprintf("SELECT c.id, c.name, c.connections, c.time_edit, g.name as level
 	FROM clients c LEFT JOIN groups g ON c.group_bits = g.id
 	WHERE c.group_bits >= 8
-	AND  c.group_bits <=64 AND(%d - c.time_edit > %d*60*60*24 )", $time, $length);
+	AND(%d - c.time_edit > %d*60*60*24 )", $time, $length);
 
 $query .= sprintf("ORDER BY %s ", $orderby);
 
